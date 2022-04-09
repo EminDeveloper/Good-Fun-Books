906181a8218542f88516ea4b15fd165ded54a989
Emin Developer <emin.f.mammadov@gmail.com>
2022-04-08 17:39:29 -0700

    connect db

--- src/main/java/com/bookstore/goodbooks/domain/Book.java
+++ src/main/java/com/bookstore/goodbooks/domain/Book.java
@@ -2,15 +2,27 @@ package com.bookstore.goodbooks.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "book")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int book_id;
+    @Column(name = "book_name")
     private String book_name;
+    @Column(name = "book_author")
     private String book_author;
+    @Column(name = "book_type")
     private String book_type;
+    @Column(name = "book_price")
     private String book_price;
+    @Column(name = "book_description")
     private String book_description;
+    @Column(name = "book_customer_id")
     private int book_customer_id;
 
 }
--- src/main/java/com/bookstore/goodbooks/repositories/UserRepository.java
+++ src/main/java/com/bookstore/goodbooks/repositories/UserRepository.java
@@ -1,11 +1,10 @@
 package com.bookstore.goodbooks.repositories;
 
 import com.bookstore.goodbooks.domain.User;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository {
 
     void addUser();
     void editUser();
